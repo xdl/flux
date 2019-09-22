0b647bf8aaa88e5e9711d86f59b96d1e6bfba95b
Eddie Li <eddie@xiaodi.li>
2019-09-22 13:56:06 +0100

    Adding additional assets, moving to storybook

---
+++ src/test/simple-example.stories.js
@@ -0,0 +1,66 @@
+export default { title: 'Simple Example' };
+
+import flux from '../flux';
+
+const init = (stageElement, inkscapeElement) => () => {
+
+  var inkscapeSvg = document.getElementById('inkscape-svg');
+  flux.init(stageElement, inkscapeElement, function(stage, library, helpers) {
+
+    const screen1 = library.Screen1();
+    const screen2 = library.Screen2();
+
+    stage.addChild(screen1);
+
+    screen1.next_screen_button.addEventListener('click', function() {
+      stage.removeChild(screen1);
+      stage.addChild(screen2);
+    });
+
+    screen2.prev_screen_button.addEventListener('click', function() {
+      stage.removeChild(screen2);
+      stage.addChild(screen1);
+    });
+
+    // show clickable areas, even when you haven't clicked on the svg element
+    window.addEventListener('click', function() {
+      helpers.showClickableAreas()
+    })
+
+    // This should be done at the last step
+    inkscapeElement.style.display = 'none';
+  })
+};
+
+export const actual = () => {
+  const container = document.createElement('div');
+
+  const stageElement = document.createElement('div');
+
+  const inkscapeElement = document.createElement('object');
+  inkscapeElement.setAttribute('type', 'image/svg+xml');
+  inkscapeElement.setAttribute('data', 'simple-example.svg');
+  inkscapeElement.addEventListener('load', init(stageElement, inkscapeElement));
+
+  container.appendChild(stageElement);
+  container.appendChild(inkscapeElement);
+
+  return container;
+};
+
+export const withBug = () => {
+  const container = document.createElement('div');
+  container.innerHTML = "<p>TODO: Fix transform displacement edge case bug where the button element was copied off Screen1 and pasted into Screen2.</p>"
+
+  const stageElement = document.createElement('div');
+
+  const inkscapeElement = document.createElement('object');
+  inkscapeElement.setAttribute('type', 'image/svg+xml');
+  inkscapeElement.setAttribute('data', 'simple-example-bug.svg');
+  inkscapeElement.addEventListener('load', init(stageElement, inkscapeElement));
+
+  container.appendChild(stageElement);
+  container.appendChild(inkscapeElement);
+
+  return container;
+};
